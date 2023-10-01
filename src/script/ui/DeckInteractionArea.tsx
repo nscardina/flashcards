1056c52794f211dd3c903c4751447fee229d4599
Noah Scardina <88603144+nscardina@users.noreply.github.com>
2023-10-01 00:00:37 -0400

    Added features to reviewing mode Added progress bar at the bottom of the screen to show progress through the deck of cards Removed wraparound behavior from arrow buttons in reviewing mode - left arrow button no longer wraps around to the end of the deck, and right arrow button exits reviewing mode at the end of the deck

--- src/script/ui/DeckInteractionArea.tsx
+++ src/script/ui/DeckInteractionArea.tsx
@@ -1,24 +1,23 @@
-import { Button, Col, Row } from "react-bootstrap"
+import { Button, Col, ProgressBar, Row } from "react-bootstrap"
 import CardDisplay from "../card/CardDisplay"
 import { useContext } from "react"
 import { AppState } from "../App"
 import { AppStateType } from "../state/AppState"
+import AppMode from "../app/AppMode"
 
 function viewPreviousCard(state: AppStateType) {
   // If there are multiple cards in the deck, and the currently visible card 
   // isn't the first card, then subtract one from the current index to get the 
   // new visible card index.
-  if (state.deck !== null && state.deck.cards.length > 1 
-    && state.visibleCardIndex > 0)
-  {
+  if (state.deck !== null && state.deck.cards.length > 1
+    && state.visibleCardIndex > 0) {
     state.setVisibleCardIndex(state.visibleCardIndex - 1)
   }
   // If there are multiple cards in the deck, and the currently visible card 
   // is the first card, then set the new visible card index to the index of the 
   // last card in the deck.
-  else if (state.deck !== null && state.deck.cards.length > 1 && 
-    state.visibleCardIndex === 0)
-  {
+  else if (state.deck !== null && state.deck.cards.length > 1 &&
+    state.visibleCardIndex === 0) {
     state.setVisibleCardIndex(state.deck.cards.length - 1)
   }
   // Otherwise, there is only one card in the deck, and the visible card index 
@@ -30,16 +29,14 @@ function viewNextCard(state: AppStateType) {
   // isn't the last card, then add one to the current index to get the new 
   // visible card index.
   if (state.deck && state.deck.cards.length > 1 &&
-    state.visibleCardIndex < state.deck.cards.length - 1)
-  {
+    state.visibleCardIndex < state.deck.cards.length - 1) {
     state.setVisibleCardIndex(state.visibleCardIndex + 1)
   }
   // If there are multiple cards in the deck, and the currently visible card
   // is the last card, then set the new visible card index to the index of the 
   // first card in the deck.
-  else if (state.deck && state.deck.cards.length > 1 && 
-    state.visibleCardIndex === state.deck.cards.length - 1)
-  {
+  else if (state.deck && state.deck.cards.length > 1 &&
+    state.visibleCardIndex === state.deck.cards.length - 1) {
     state.setVisibleCardIndex(0)
   }
   // Otherwise, there is only one card in the deck, and the visible card index 
@@ -51,35 +48,58 @@ function DeckInteractionArea() {
   const appState = useContext(AppState)
 
   return (
-    <Row className="mt-3 d-flex">
-      <Col xs={{ span: 6, order: 2 }} md={{ span: 1, order: 1 }}
-        className="d-flex align-items-center justify-content-center"
-      >
-        <Button 
-          disabled={appState.deck === null || appState.deck.cards.length <= 1} 
-          className="d-flex align-items-center flashcard-button flashcard-round-button"
-          onClick={() => viewPreviousCard(appState)}
+    <>
+      <Row className="mt-3 d-flex">
+        <Col xs={{ span: 6, order: 2 }} md={{ span: 1, order: 1 }}
+          className="d-flex align-items-center justify-content-center"
         >
-          <span className="material-symbols-outlined">
-            arrow_back
-          </span>
-        </Button>
-      </Col>
-      <Col xs={{ span: 12, order: 1 }} md={{ span: 10, order: 2 }}>
-        <CardDisplay />
-      </Col>
-      <Col xs={{ span: 6, order: 3 }} md={{ span: 1, order: 3 }} className="d-flex align-items-center justify-content-center">
-        <Button 
-          className="d-flex align-items-center flashcard-button flashcard-round-button"
-          disabled={appState.deck === null || appState.deck.cards.length <= 1} 
-          onClick={() => viewNextCard(appState)}
-        >
-          <span className="material-symbols-outlined">
-            arrow_forward
-          </span>
-        </Button>
-      </Col>
-    </Row>
+          <Button
+            disabled={appState.deck === null || appState.deck.cards.length <= 1}
+            className="d-flex align-items-center flashcard-button flashcard-round-button"
+            onClick={() => {
+              if (appState.appMode !== AppMode.REVIEWING_DECK || 
+                appState.visibleCardIndex !== 0) {
+                viewPreviousCard(appState)
+              }
+            }}
+          >
+            <span className="material-symbols-outlined">
+              arrow_back
+            </span>
+          </Button>
+        </Col>
+        <Col xs={{ span: 12, order: 1 }} md={{ span: 10, order: 2 }}>
+          <CardDisplay />
+        </Col>
+        <Col xs={{ span: 6, order: 3 }} md={{ span: 1, order: 3 }} className="d-flex align-items-center justify-content-center">
+          <Button
+            className="d-flex align-items-center flashcard-button flashcard-round-button"
+            disabled={appState.deck === null || appState.deck.cards.length <= 1}
+            onClick={() => {
+              if (appState.appMode !== AppMode.REVIEWING_DECK || (
+                appState.deck && 
+                appState.visibleCardIndex !== appState.deck.cards.length - 1
+              )) {
+                viewNextCard(appState)
+              } else {
+                appState.setAppMode(AppMode.MANAGING_FILES)
+              }
+              
+            }}
+          >
+            <span className="material-symbols-outlined">
+              arrow_forward
+            </span>
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mt-3 d-flex">
+        {appState.appMode === AppMode.REVIEWING_DECK && appState.deck && 
+        <ProgressBar now={(appState.visibleCardIndex + 1) / appState.deck.cards.length * 100} 
+        label={`Card ${appState.visibleCardIndex + 1} of ${appState.deck.cards.length}`} />
+        }
+      </Row>
+    </>
   )
 
 }
