cd5139ec2f65161756bab5d728d9944bc8c62208
Noah Scardina <88603144+nscardina@users.noreply.github.com>
2024-09-09 00:06:43 +0000

    fix: Fixed errors preventing vite build from working

--- src/script/file/CardFile.tsx
+++ src/script/file/CardFile.tsx
@@ -6,24 +6,19 @@ export function downloadDeck(deck: Deck) {
     const linkElem = document.createElement("a");
 
     const nameEndsInNewline = (deck.name.at(-1) == "\n");
-    const trimmedName = `${deck.name.substring(0, deck.name.length - 2)}.deck`
-    const untrimmedName = `${deck.name}.deck`
 
     const options: Options = {
         replacement: "_"
     }
 
     const name = nameEndsInNewline
-    ? trimmedName
-    : untrimmedName;
+    ? `${deck.name.substring(0, deck.name.length - 2)}.deck`
+    : `${deck.name}.deck`;
 
-    filenamify(
-        "test"
+    linkElem.download = filenamify(
+        name,
+        options
     )
-
-    console.log(test);
-
-    linkElem.download = 
     linkElem.href = `data:text/plain,${encodeURIComponent(JSON.stringify(deck))}`
 
     document.body.appendChild(linkElem)
@@ -36,7 +31,10 @@ export async function uploadDeck(files: FileList): Promise<Deck> {
 
     const file = files.item(0)
     if (file !== null) {
-        return JSON.parse(await file.text())
+        const parsedJSON: unknown = JSON.parse(await file.text());
+        if (Deck.isDeck(parsedJSON)) {
+            return parsedJSON;
+        }
     }
 
     throw new Error("Unable to load deck")
